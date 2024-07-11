643f96d5b7846adb00c878138f19eeb0c1ecb604
sandeep-rana-19 <96654527+sandeeprana191@users.noreply.github.com>
2024-07-11 08:14:57 +0530

    episode-09 lip

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
   const [btnNameReact, setBtnNameReact] = useState("Login");
+  const onlineStatus = useOnlineStatus();
 
   useState(() => {}, [btnNameReact]);
 
@@ -14,6 +16,7 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
+          <li>Online status:{onlineStatus ? "✅" : "🔴"}</li>
           <li>
             <Link to="/">Home</Link>
           </li>
