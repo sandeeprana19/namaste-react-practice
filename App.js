0208ee2e495ca5d04b8b3306a8fdf5e7b52ccae2
Sandeep Rana <62502757+sandeeprana19@users.noreply.github.com>
2025-02-13 03:09:17 +0530

    main
    ep-2

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
+    React.createElement("h1", {}, "This is Namaste React 🚀"),
+    React.createElement("h2", {}, "sdadasdasd"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I'm h1 tag"),
