e16556f4dc30d2732b390b03c2c720b7d7fc54a2
Sandeep Rana <62502757+sandeeprana19@users.noreply.github.com>
2025-05-05 01:50:56 +0530

    e-03

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/**
+ *
+ * Header
+ *  - Logo
+ *  - Nav items
+ * Body
+ *  - Restaurant Container
+ *    - Restaurant Card
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ *
+ */
+
+// const AppLayout=()
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
